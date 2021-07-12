6fb4787d9188aa0f9fcff623a592fb017ac32b85
Ju Dantas <jucienyds@gmail.com>
2021-07-12 05:40:07 -0300

    chore: configurado eslint

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,7 +1,7 @@
-import { User } from "@models/User";
+import { User } from '@models/User'
 
 export class UsersController {
-    test(){
-        const user = new User()
-    }
+  test () {
+    const user = new User()
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import express from 'express'
 const server = express()
 const port = process.env.PORT || 3333
 
-server.get("/", (req, res) => {
-    return res.json({ message: "it's works!" })
+server.get('/', (req, res) => {
+  return res.json({ message: "it's works!" })
 })
 
 server.listen(port, () => {
-    console.log(`Server's running in http://localhost:${port}`)
+  console.log(`Server's running in http://localhost:${port}`)
 })
