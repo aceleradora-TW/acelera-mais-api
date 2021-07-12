cd3af70ee0267c2c09159aa7d5335240662de143
Ju Dantas <jucienyds@gmail.com>
2021-07-12 06:24:11 -0300

    chore: configura babel para comando build

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@controllers': './src/controllers',
+        '@models': './src/models'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
