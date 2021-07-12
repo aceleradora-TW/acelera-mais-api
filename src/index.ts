547b60921027da5daf1c29b40ffbf86ac4db14fc
Ju Dantas <jucienyds@gmail.com>
2021-07-11 22:11:57 -0300

    chore: adiciona typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const server = express()
+const port = process.env.PORT || 3333
+
+server.get("/", (req, res) => {
+    return res.json({ message: "it's works!" })
+})
+
+server.listen(port, () => {
+    console.log(`Server's running in http://localhost:${port}`)
+})
