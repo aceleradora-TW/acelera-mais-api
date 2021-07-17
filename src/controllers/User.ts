b9998bb745f3a3cdc8e3cdc3684729a7bb928fb1
Ju Dantas <jucienyds@gmail.com>
2021-07-17 20:01:34 -0300

    chore: trabalho em andamento
    estou organizando o typeORM

---
+++ src/controllers/User.ts
@@ -0,0 +1,13 @@
+import { getRepository } from 'typeorm'
+import { User } from '@models/User'
+
+export const getAllUsers = (request, response) => {
+  const users = getRepository(User).find()
+  return response.json(users)
+}
+
+export const getUser = (request, response) => {
+  const { id } = request.params
+  const user = getRepository(User).findByIds([id])
+  return response.json(user)
+}
