02da9198e52ad6e9d8cf0d1884736e4174d5bc75
luiz Henrique <luizlhra24@gmail.com>
2021-10-01 14:58:59 -0300

    feat(auth): cria autenticação

---
+++ src/controllers/auth/index.ts
@@ -0,0 +1,12 @@
+export const login = (request, response) => {
+  const email = request.body.email
+  const password = request.body.password
+  const emailUser = 'ju@gmail.com'
+  const passwordUser = '123456'
+  if (email === emailUser && password === passwordUser) {
+    return response.json({ message: 'Bem vinda querida Ju!' })
+  }
+
+  return response.json({ message: 'tente novamente!' })
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import './models'
 import express from 'express'
 import { itsWorks } from './controllers'
-
+import { login } from './controllers/auth'
 const app = express()
 const port = process.env.PORT || 9000
 
-app.get('/', itsWorks)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+app.get('/', itsWorks)
+app.post('/login', login)
 app.listen(port, () => {
   console.log(`Server's running in http://localhost:${port}`)
 })
