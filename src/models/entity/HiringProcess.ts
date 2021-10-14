098e398aba9b1cde41c74637be5bb18863fb9fcb
Ju Dantas <jucienyds@gmail.com>
2021-10-14 15:23:58 -0300

    fix: adiciona label ao atributo name

--- src/models/entity/HiringProcess.ts
+++ src/models/entity/HiringProcess.ts
@@ -3,38 +3,38 @@ import { IsNotEmpty, IsDate } from 'class-validator'
 import { Match } from '@models/validators/date'
 @Entity()
 export class HiringProcess {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    @IsNotEmpty()
-    name: string;
+  @Column({ name: 'name' })
+  @IsNotEmpty()
+  name: string;
 
-    @Column({ name: 'start_date', type: 'timestamptz' })
-    @IsNotEmpty()
-    @IsDate()
-    startDate: Date;
+  @Column({ name: 'start_date', type: 'timestamptz' })
+  @IsNotEmpty()
+  @IsDate()
+  startDate: Date;
 
-    @Column({ name: 'end_date', type: 'timestamptz' })
-    @IsDate()
-    @Match('startDate')
-    endDate: Date;
+  @Column({ name: 'end_date', type: 'timestamptz' })
+  @IsDate()
+  @Match('startDate')
+  endDate: Date;
 
-    @Column()
-    description: string;
+  @Column()
+  description: string;
 
-    @CreateDateColumn({
-      name: 'created_at',
-      type: 'timestamptz',
-      default: () => 'CURRENT_TIMESTAMP(6)'
-    })
-    createdAt: Date;
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP(6)'
+  })
+  createdAt: Date;
 
-    @UpdateDateColumn({
-      name: 'updated_at',
-      type: 'timestamptz',
-      default: () => 'CURRENT_TIMESTAMP(6)',
-      onUpdate: 'CURRENT_TIMESTAMP(6)'
-    })
-    updatedAt: Date;
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)'
+  })
+  updatedAt: Date;
 }
