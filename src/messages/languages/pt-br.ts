3d856a64840e12d9cf558e8f067d69d249373663
Jorel Martins <bearshamis@gmail.com>
2021-10-28 15:31:12 -0300

    feat(hiring process): acrescentamos menssagem de erro 410 e message not removed
    @jojorel @janinelps
    
    BREAKING CHANGE: 15
    
    15

--- src/messages/languages/pt-br.ts
+++ src/messages/languages/pt-br.ts
@@ -1,4 +1,5 @@
 export const message = {
   SUCCESS: 'Salvo com sucesso!',
-  REMOVED: 'Removido com sucesso!'
+  REMOVED: 'Removido com sucesso!',
+  NOT_REMOVED: 'Recurso não pode ser removido.'
 }
