4561b2e6eb8ee85d469588adf6436135dd4add8b
Bianca Salino <biancadsalino@gmail.com>
2021-11-01 10:37:14 -0300

    fix(hiring-process): arrumando conflito no merge
    @ccarolb @biancasalino @jojorel
    
    20

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import './models'
 import express from 'express'
 import { login } from '@controllers/auth'
-<<<<<<< HEAD
-import { createProcess, editHiringProcess } from '@controllers/hiring-process'
-=======
-import { createHiringProcess, getAllHiringProcesses } from '@controllers/hiring-process'
->>>>>>> 0c06d9f5b5ef80c510f5cf2ea64a478abbccb3c6
+import { createHiringProcess, getAllHiringProcesses, editHiringProcess } from '@controllers/hiring-process'
 import cors from 'cors'
 import { itsWorks } from './controllers'
 
@@ -18,14 +14,9 @@ app.use(cors({ origin: '*' }))
 
 app.get('/', itsWorks)
 app.post('/login', login)
-<<<<<<< HEAD
-app.post('/hiring_process', createProcess)
 app.patch('/hiring_process/:id', editHiringProcess)
-
-=======
 app.post('/hiring_process', createHiringProcess)
 app.get('/hiring_process', getAllHiringProcesses)
->>>>>>> 0c06d9f5b5ef80c510f5cf2ea64a478abbccb3c6
 app.listen(port, () => {
   console.log(`Server's running in http://localhost:${port}`)
 })
