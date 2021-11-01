0b7b1d7463c67b7d0281ef3a9204bf8a5c9cf07e
Carolina Borges <84879029+ccarolb@users.noreply.github.com>
2021-11-01 10:56:57 -0300

    Revert "20/api editar processo seletivo"

--- src/messages/languages/pt-br.ts
+++ src/messages/languages/pt-br.ts
@@ -1,6 +1,3 @@
 export const message = {
-  SUCCESS: 'Salvo com sucesso!',
-  UPDATED: 'Atualizado com sucesso!',
-  NOT_FOUND: 'Não foram encontradas informações.'
-
+  SUCCESS: 'Salvo com sucesso!'
 }
