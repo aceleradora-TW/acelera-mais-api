08c6accc6cf2ac68f5a14ec7cea953262969e9ee
janinelps <janinelpss@gmail.com>
2021-11-08 15:28:59 -0300

    feat(credencial): variavel contendo id da url
    @biamah @janinelps @jojorel @leonardogomesp @luizhenrique000

--- src/controllers/spreadsheets/index.ts
+++ src/controllers/spreadsheets/index.ts
@@ -1,2 +1,9 @@
-const urlTable = new GoogleSpreadsheets("https://docs.google.com/spreadsheets/d/1FxkDqFIfCAwvwrwmR-BMCuV_uO_YQv1H8U7QHA-FJk4/edit#gid=0")
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const { URL } = require('url')
 
+const link = new URL('https://docs.google.com/spreadsheets/d/1FxkDqFIfCAwvwrwmR-BMCuV_uO_YQv1H8U7QHA-FJk4/edit#gid=0')
+const idLink = link.pathname.split('/')[3]
+
+const urlTable = new GoogleSpreadsheet(idLink).spreadsheetId
+
+console.log(urlTable)
