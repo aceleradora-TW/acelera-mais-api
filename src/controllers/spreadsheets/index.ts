6dd12b17d2eab6b731ac27601c26145f6d8d352d
Biamah <beatriz.souza.costa@outlook.com>
2021-11-09 10:50:57 -0300

    feat(controllers spreadsheets): importacao em andamento
    @biamah @janinelps

--- src/controllers/spreadsheets/index.ts
+++ src/controllers/spreadsheets/index.ts
@@ -1,9 +1,21 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet')
-const { URL } = require('url')
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import { URL } from 'url'
+import credential from '../../credential.json'
 
 const link = new URL('https://docs.google.com/spreadsheets/d/1FxkDqFIfCAwvwrwmR-BMCuV_uO_YQv1H8U7QHA-FJk4/edit#gid=0')
 const idLink = link.pathname.split('/')[3]
 
-const urlTable = new GoogleSpreadsheet(idLink).spreadsheetId
+const connectSheet = async (idSheet) => {
+  const docSheet = new GoogleSpreadsheet(idSheet)
+  await docSheet.useServiceAccountAuth({ private_key: credential.private_key, client_email: credential.client_email })
+  return docSheet
+}
 
-console.log(urlTable)
+const loadDocument = async (idSheet) => {
+  const connection = connectSheet(idSheet)
+  await connection.loadInfo()
+  return connection.title
+}
+
+connectSheet(idLink)
+console.log(loadDocument(idLink))
