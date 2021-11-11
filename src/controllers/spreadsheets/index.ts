4bf0f588a7fe28de7b6546368125b09e6acff95a
Biamah <beatriz.souza.costa@outlook.com>
2021-11-11 15:55:11 -0300

    feat(models entity spreadsheet): criando colunas em andamento
    @janinelps @biamah

--- src/controllers/spreadsheets/index.ts
+++ src/controllers/spreadsheets/index.ts
@@ -10,7 +10,7 @@ const connectSheet = async (idSheet) => {
 
 export const connection = async (request, response) => {
   console.log(request.body)
-  const { urlSheet } = request.body
+  const { urlSheet, hiringProcessID } = request.body
   try {
     const link = new URL(urlSheet)
     const idLink = link.pathname.split('/')[3]
@@ -19,22 +19,23 @@ export const connection = async (request, response) => {
     const sheetFirst = sheetConnection.sheetsByIndex[0]
     const spreadsheets = (await sheetFirst.getRows()).map((row) => {
       return {
-        id: row._rawData[0],
-        TimeStamp: row._rawData[1],
+        timeStamp: row._rawData[1],
         adressEmail: row._rawData[2],
         name: row._rawData[3],
         email: row._rawData[4],
         phone: row._rawData[5],
         birthDate: row._rawData[6],
-        instituitionName: row._rawData[7],
-        courseName: row._rawData[8],
-        milestone: row._rawData[9],
-        howFound: row._rawData[10],
-        expectation: row._rawData[11],
-        motivation: row._rawData[12],
-        curriculum: row._rawData[13],
-        okCI: row._rawData[14]
-
+        genre: row._rawData[7],
+        skinColor: row._rawData[8],
+        instituitionName: row._rawData[9],
+        courseName: row._rawData[10],
+        milestone: row._rawData[11],
+        howFound: row._rawData[12],
+        expectation: row._rawData[13],
+        motivation: row._rawData[14],
+        curriculum: row._rawData[15],
+        okCI: row._rawData[16],
+        hiringProcessID
       }
     })
 
--- src/models/entity/Spreadsheet.ts
+++ src/models/entity/Spreadsheet.ts
@@ -1,27 +1,64 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm'
-import { IsNotEmpty } from 'class-validator'
+import { IsDate, IsNotEmpty } from 'class-validator'
 
 @Entity()
 export class Spreadsheet {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ name: 'time_stamp', type: 'timestamptz' })
+  @IsNotEmpty()
+  @IsDate()
+  timeStamp: Date;
+
+  @Column({ name: 'adress_email', type: 'varchar' })
+  @IsNotEmpty()
+  adressEmail: string;
+
   @Column({ name: 'name', type: 'varchar' })
   @IsNotEmpty()
   name: string;
 
-  @CreateDateColumn({
-    name: 'created_at',
-    type: 'timestamptz',
-    default: () => 'CURRENT_TIMESTAMP(6)'
-  })
-  createdAt: Date;
+  @Column({ name: 'hiring_process_id', type: 'number' })
+  @IsNotEmpty()
+  hiringProcessID: number;
 
-  @UpdateDateColumn({
-    name: 'updated_at',
-    type: 'timestamptz',
-    default: () => 'CURRENT_TIMESTAMP(6)',
-    onUpdate: 'CURRENT_TIMESTAMP(6)'
-  })
-  updatedAt: Date;
+  @Column({ name: 'email', type: 'varchar' })
+  @IsNotEmpty()
+  email: string;
+
+  @Column({ name: 'phone', type: 'varchar' })
+  @IsNotEmpty()
+  phone: string;
+
+  @Column({ name: 'birth_date', type: 'timestamptz' })
+  @IsNotEmpty()
+  birthDate: Date;
+
+  @Column({ name: 'instituition_name', type: 'varchar' })
+  @IsNotEmpty()
+  instituitionName: string;
+
+  @Column({ name: 'course_name', type: 'varchar' })
+  @IsNotEmpty()
+  courseName: string;
+
+  @Column({ name: 'milestone', type: 'varchar' })
+  @IsNotEmpty()
+  milestone: string;
+
+  // @CreateDateColumn({
+  //   name: 'created_at',
+  //   type: 'timestamptz',
+  //   default: () => 'CURRENT_TIMESTAMP(6)'
+  // })
+  // createdAt: Date;
+
+  // @UpdateDateColumn({
+  //   name: 'updated_at',
+  //   type: 'timestamptz',
+  //   default: () => 'CURRENT_TIMESTAMP(6)',
+  //   onUpdate: 'CURRENT_TIMESTAMP(6)'
+  // })
+  // updatedAt: Date;
 }
