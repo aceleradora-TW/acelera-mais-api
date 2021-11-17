3dedb160b75f4dded7ccfca5e1a9c719a6b72e97
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-11-17 15:56:09 -0300

    feat(service/exercise-review): implementa condição de score
    @eluvya @ccarolb @patriciatersariol
    
    48

--- src/service/exercise-reviewed/ExerciseReviewedRequest.ts
+++ src/service/exercise-reviewed/ExerciseReviewedRequest.ts
@@ -1,9 +1,9 @@
 export class ExerciseReviewedRequest {
   public readonly name: string;
   public readonly feedback: string;
-  public readonly score: number;
+  public readonly score: string;
 
-  constructor(name: string, feedback: string, score: number) {
+  constructor(name: string, feedback: string, score: string) {
     this.name = name
     this.feedback = feedback
     this.score = score
@@ -11,14 +11,10 @@ export class ExerciseReviewedRequest {
 
   public static convertFromHttpBody(body) {
     const { name, feedback, score } = body
-    if (score >= 0 && score <= 5) {
-      return new ExerciseReviewedRequest(
-        name,
-        feedback,
-        score
-      )
-    } else {
-      return 'Score should be between 0 and 5'
-    }
+    return new ExerciseReviewedRequest(
+      name,
+      feedback,
+      score >= 0 && score <= 5 ? score : 'Score should be between 0 and 5'
+    )
   }
 }
