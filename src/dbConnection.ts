4396d343159a84f2f3b4a8136872d4484ebf6941
Leonardo Dias <Ldias@gap.com>
2021-11-17 22:23:07 -0300

    refact: db connection

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -4,35 +4,12 @@ import { createConnection, ConnectionOptions } from 'typeorm'
 const connect = async () => {
   const local = !process.env.DATABASE_URL
   const url = process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/aceleradora_agil'
-  console.log('DB_URL:' + url)
-  const optionsSsl: ConnectionOptions = {
-    type: 'postgres',
-    name: 'default',
-    url,
-    ssl: {
-      rejectUnauthorized: false
-    },
-    synchronize: true,
-    entities: ['**/models/**/*{.ts,.js}'],
-    migrations: [
-      'src/models/migration/**/*.ts'
-    ],
-    subscribers: [
-      'src/models/subscriber/**/*.ts'
-    ],
-    cli: {
-      entitiesDir: 'src/models/entity',
-      migrationsDir: 'src/models/migration',
-      subscribersDir: 'src/models/subscriber'
-    }
-  }
 
   const options: ConnectionOptions = {
     type: 'postgres',
     name: 'default',
     url,
     synchronize: true,
-    logging: true,
     entities: ['**/models/**/*{.ts,.js}'],
     migrations: [
       'src/models/migration/**/*.ts'
@@ -47,6 +24,8 @@ const connect = async () => {
     }
   }
 
-  await createConnection(local ? options : optionsSsl)
+  await createConnection(local
+    ? options
+    : { ...options, ssl: { rejectUnauthorized: false } })
 }
 connect()
