891837034006563e37df1028c01e60b41981f653
Ju Dantas <jucienyds@gmail.com>
2021-11-20 00:19:42 -0300

    test: comenta antigo endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,7 @@ import {
   editHiringProcess,
   getAllHiringProcesses
 } from '@controllers/hiring-process'
-import { importSpreadsheet } from '@controllers/spreadsheet'
+// import { importSpreadsheet } from '@controllers/spreadsheet'
 import { importCandidates } from '@controllers/candidate'
 
 export const defineRoutes = (app) => {
@@ -17,6 +17,6 @@ export const defineRoutes = (app) => {
   app.get('/hiring_process', getAllHiringProcesses)
   app.delete('/hiring_process/:id', delAllHiringProcesses)
 
-  app.post('/candidates', importSpreadsheet)
+  // app.post('/candidates', importSpreadsheet)
   app.post('/hiring_process/:id/candidate', importCandidates)
 }
