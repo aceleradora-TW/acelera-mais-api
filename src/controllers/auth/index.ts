3a1b78f8c22372c54d2afb388e57583595e02407
patricia <patriciatersariol@gmail.com>
2021-11-23 15:28:52 -0300

    feat(auth): retira expiracao do token
    @patriciatersariol @eluvya
    
    64

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -4,20 +4,17 @@ export const generateAccessToken = (request, response) => {
   const { EMAIL_ADMIN, PASSWORD_ADMIN, NAME_ADMIN, SECRET } = process.env
   const emailUser = request.body.email
   const passwordUser = request.body.password
-  console.log(EMAIL_ADMIN)
 
   if (emailUser !== EMAIL_ADMIN || passwordUser !== PASSWORD_ADMIN) {
     return response.sendStatus(401)
   }
 
   const payload = { name: NAME_ADMIN, email: EMAIL_ADMIN }
-  const accessToken = jwt.sign(payload, SECRET, {
-    expiresIn: 3600
-  })
+  const accessToken = jwt.sign(payload, SECRET)
 
   return response.json({
     auth: true,
-    accessToken: accessToken,
+    accessToken,
     user: payload
   })
 }
