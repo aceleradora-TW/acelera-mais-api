e23691f1738d1fb917d7c2d71e38d5225c355fd9
Cibely Pereira <cibely.port@gmail.com>
2021-11-30 13:53:14 -0300

    feat(controllers/exercises): cria mapexercises em andamento
    @Cibely87 @biamah

--- src/controllers/exercises/index.ts
+++ src/controllers/exercises/index.ts
@@ -5,7 +5,19 @@ const mapExercises = (id) => {
   return (rows) => {
     return rows.map(r => {
       return {
-        
+        timeStamp: r['Carimbo de data/hora'],
+        addressEmail: r['Endereço de e-mail'],
+        name: r['Informe seu nome completo'],
+        phone: r['Informe o número de um telefone para contato:'],
+        exercise: r['Informe qual o exercício que você escolheu:'],
+        fileType: r['O que você prefere nos enviar?'],
+        fileZip: r['Arquivo . ZIP'],
+        fileGithub: r['Nos informe o link completo do seu repositório no GitHub com a solução do exercício.'],
+        haveComputer: r['Você possui computador em casa ?'],
+        haveInternet: r['Você possui acesso a internet em casa?'],
+        haveWebcam: r['Voce Possui Webcam?'],
+        canUserWebcam: r['Você se incomodaria em abrir sua Webcam durante as interações quanto a Aceleradora Ágil?'],
+        cityState: r['Qual a sua cidade/estado?']
       }
     })
   }
@@ -16,6 +28,6 @@ export const importExercises = async (request, response) => {
   const { link } = request.body
 
   const exercisesSheet = await importSpreadSheet(link, mapExercises(id))
-
+  console.log(mapExercises(id))
   return response.json({ message: "Oi Cibely!" })
 }
