595edca8afc0e7eceac2c15e700c0fa9cc9eda90
Cibely Pereira <cibely.port@gmail.com>
2021-11-30 11:51:02 -0300

    feat(controllers\exercises): cria mapExercises em andamento
    @Cibely87 @biamah

--- src/controllers/exercises/index.ts
+++ src/controllers/exercises/index.ts
@@ -1,4 +1,21 @@
+import { importSpreadSheet } from "src/service/google-spreadsheet"
+
+
+const mapExercises = (id) => {
+  return (rows) => {
+    return rows.map(r => {
+      return {
+        
+      }
+    })
+  }
+}
+
 export const importExercises = async (request, response) => {
-    //const { link } = request.body
-    return response.json({ message: "Oi Cibely!" })
+  const { id } = request.params
+  const { link } = request.body
+
+  const exercisesSheet = await importSpreadSheet(link, mapExercises(id))
+
+  return response.json({ message: "Oi Cibely!" })
 }
