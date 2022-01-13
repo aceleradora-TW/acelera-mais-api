61a65308a8ff389c193425dbe6e41b3e3c326763
estherwl <estherlucena@gmail.com>
2022-01-13 11:46:28 -0300

    refactor(service/exercise): altera classe ExerciseService para funcao
    @estherwl
    
    121

--- src/service/exercise/ExerciseService.ts
+++ src/service/exercise/ExerciseService.ts
@@ -3,8 +3,26 @@ import { Exercise } from "@models/entity/Exercise"
 import { HttpError, HttpStatusCode } from "@service/HttpError"
 import { getRepository } from "typeorm"
 
-export class ExerciseService {
-  public async getAllExercises({ page, count, hiringProcessId, type }) {
+// export class ExerciseService {
+//   public async getAllExercises({ page, count, hiringProcessId, type }) {
+//     let where = { hiringProcess: hiringProcessId }
+//     if (type) { where = { ...where, type } }
+//     const exerciseRepository = getRepository(Exercise)
+//     const result = await exerciseRepository.find({
+//       where,
+//       skip: page,
+//       take: count
+//     })
+//     if (result.length === 0) {
+//       throw new HttpError(message.NOT_FOUND, HttpStatusCode.NOT_FOUND)
+//     }
+//     return result
+//   }
+// }
+
+export const exerciseService = () => {
+
+  const getAllExercises = async ({ page, count, hiringProcessId, type }) => {
     let where = { hiringProcess: hiringProcessId }
     if (type) { where = { ...where, type } }
     const exerciseRepository = getRepository(Exercise)
@@ -18,4 +36,6 @@ export class ExerciseService {
     }
     return result
   }
+
+  return { getAllExercises }
 }
