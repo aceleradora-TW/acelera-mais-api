6f8eefef65fc6a2818f510f39f1b2d7e8d0cab25
Carolina Borges <carolinaborgescardoso@gmail.com>
2022-01-14 11:58:43 -0300

    refactor(Service/EvaluationService): transforma classes em funções
    @ccarolb @biancasalino
    
    120

--- src/service/evaluation/EvaluationService.ts
+++ src/service/evaluation/EvaluationService.ts
@@ -2,55 +2,58 @@ import { getRepository } from 'typeorm'
 import { validate } from 'class-validator'
 import { Evaluation } from '@models/entity/Evaluation'
 import { HttpError, HttpStatusCode } from '../HttpError'
+import { evaluationRequest } from './EvaluationRequest'
 
-export const EvaluationService {
-  const  createEvaluationService = (evaluationRequest: any) {
-    try
+export const evaluationService = () => {
+
+
+  const createEvaluationService = async (evaluationRequest) => {
+
     const evaluationRepository = getRepository(Evaluation)
     const evaluationEntity = await evaluationRepository.create(evaluationRequest)
-    await this.validateEvaluation(evaluationEntity)
-    const evaluationEntitySaved = await evaluationRepository.save(evaluationEntity)
+    const evaluationValidated = await validateEvaluation(evaluationEntity)
+    const evaluationEntitySaved = await evaluationRepository.save(evaluationValidated)
     return evaluationEntitySaved
   }
 
-  private async validateEvaluation(evaluation) {
-  const errors = await validate(evaluation)
-  if (errors.length > 0) {
-    throw new HttpError('Errors validating the evaluation:' + errors, HttpStatusCode.BAD_REQUEST)
-  }
-}
-
-  public async editEvaluation(id, mentorName, score, feedback) {
-  const evaluationRepository = getRepository(Evaluation)
-  const evaluation = await evaluationRepository.findOne(id)
-  if (!evaluation) {
-    throw new HttpError('Evaluation not found with: ' + id, HttpStatusCode.BAD_REQUEST)
+  const validateEvaluation = async ({ evaluation }) => {
+    const errors = await validate(evaluation)
+    if (errors.length > 0) {
+      throw new HttpError('Errors validating the evaluation:' + errors, HttpStatusCode.BAD_REQUEST)
+    }
   }
 
-  if (mentorName) {
-    evaluation.mentorName = mentorName
+  const editEvaluation = async ({ id, mentorName, score, feedback }) => {
+    const evaluationRepository = getRepository(Evaluation)
+    const evaluation = await evaluationRepository.findOne(id)
+    if (!evaluation) {
+      throw new HttpError('Evaluation not found with: ' + id, HttpStatusCode.BAD_REQUEST)
+    }
+
+    if (mentorName) {
+      evaluation.mentorName = mentorName
+    }
+
+    if (score) {
+      evaluation.score = score
+    }
+
+    if (feedback) {
+      evaluation.feedback = feedback
+    }
+
+    this.validateEvaluation(evaluation)
+    const result = await evaluationRepository.save(evaluation)
+    return result
   }
 
-  if (score) {
-    evaluation.score = score
+  const deleteEvaluation = async ({ id }) => {
+    const evaluationRepository = getRepository(Evaluation)
+    const evaluationDeleted = await evaluationRepository.delete(id)
+
+    if (evaluationDeleted.affected === 0) {
+      throw new HttpError('Evaluation not found with: ' + id, HttpStatusCode.NOT_FOUND)
+    }
+    return evaluationDeleted
   }
-
-  if (feedback) {
-    evaluation.feedback = feedback
-  }
-
-  this.validateEvaluation(evaluation)
-  const result = await evaluationRepository.save(evaluation)
-  return result
-}
-
-  public async deleteEvaluation(id) {
-  const evaluationRepository = getRepository(Evaluation)
-  const evaluationDeleted = await evaluationRepository.delete(id)
-
-  if (evaluationDeleted.affected === 0) {
-    throw new HttpError('Evaluation not found with: ' + id, HttpStatusCode.NOT_FOUND)
-  }
-  return evaluationDeleted
-}
 }
