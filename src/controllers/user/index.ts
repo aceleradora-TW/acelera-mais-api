b103573d40665ed6201b1e46b3eb10c8b94cca3d
leonardo gomes <leonardogomespadilha@gmail.com>
2022-05-24 09:33:09 -0300

    feat(controller/user): implementa função delete em /user - @leonardogomesp @jcdantss

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,7 +1,9 @@
 import { httpResponseHandler } from "@controllers/HttpResponseHandler"
 import { message } from "@messages/languages/pt-br"
+import { User } from "@models/entity/User"
 import { userRequest } from "@service/user/UserRequest"
 import { userService } from "@service/user/UserService"
+import { getRepository } from "typeorm"
 
 
 export const createUser = async (request, response) => {
@@ -11,5 +13,18 @@ export const createUser = async (request, response) => {
     return httpResponseHandler().createSuccessResponse(message.SUCCESS, result, response)
   } catch (error) {
     return httpResponseHandler().createErrorResponse(error, response)
+  }
+}
+
+export const deleteUser = async (request, response) => {
+  try {
+    const UserRepository = getRepository(User)
+    const result = await UserRepository.delete(request.params.id)
+    if (result.affected === 0) {
+      return response.status(410).json({ message: message.NOT_REMOVED })
+    }
+    return response.json({ message: message.REMOVED, result })
+  } catch (error) {
+    return response.status(500).json(error)
   }
 }
