13c899141b3a2d664ffc43a9abd57fb1b935db74
yrachid <anuar.yasser@gmail.com>
2022-06-01 14:26:11 +0100

    chore: adiciona modulo de logger

---
+++ src/logger.ts
@@ -0,0 +1,27 @@
+
+import winston from 'winston'
+import expressWinston from 'express-winston'
+import { Handler } from 'express'
+
+const transports = [new winston.transports.Console()]
+
+const format = winston.format.json()
+
+const logger = winston.createLogger({ transports, format })
+
+const requestLogger = (): Handler => expressWinston.logger({
+    transports,
+    format,
+    meta: true,
+    msg: '{{req.method}} {{req.url}}',
+    expressFormat: false,
+    colorize: false,
+    requestWhitelist: ['body']
+})
+
+const requestErrorLogger = () => expressWinston.errorLogger({ 
+    transports, 
+    format,
+})
+
+export { requestLogger, requestErrorLogger, logger }
