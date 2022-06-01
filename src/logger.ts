62be16785798bb561b44d94d877f6d1dc7003f8f
yrachid <anuar.yasser@gmail.com>
2022-06-01 15:17:31 +0100

    chore: adiciona formatacao aos logs

--- src/logger.ts
+++ src/logger.ts
@@ -5,7 +5,11 @@ import { Handler } from 'express'
 
 const transports = [new winston.transports.Console()]
 
-const format = winston.format.json()
+const format = winston.format.combine(
+    winston.format.cli(),
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => `${timestamp} ${level}: ${message}`)
+)
 
 const logger = winston.createLogger({ transports, format })
 
@@ -13,7 +17,7 @@ const requestLogger = (): Handler => expressWinston.logger({
     transports,
     format,
     meta: true,
-    msg: '{{req.method}} {{req.url}}',
+    msg: '{{req.method}} {{req.url}} body: {{JSON.stringify(req.body)}}, headers: {{JSON.stringify(req.headers)}}',
     expressFormat: false,
     colorize: false,
     requestWhitelist: ['body']
