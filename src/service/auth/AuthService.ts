978ad23763b75510230f8d57a370c2d64baafff7
leonardo gomes <leonardogomespadilha@gmail.com>
2022-06-07 14:54:23 -0300

    feat(service/auth): implementa lógica para forçar troca de senha

--- src/service/auth/AuthService.ts
+++ src/service/auth/AuthService.ts
@@ -1,40 +1,40 @@
-import { HttpError, HttpStatusCode } from '../HttpError'
-import { findUserByEmail } from './AuthRequest'
+import { HttpError, HttpStatusCode } from "../HttpError"
+import { findUserByEmail } from "./AuthRequest"
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken")
 
 export const createAccessToken = async (emailUser, passwordUser) => {
   const { SECRET, NODEMAILER_SECRET } = process.env
+  let auth = true
 
   const encodePassword = jwt.sign(passwordUser, NODEMAILER_SECRET)
 
   const user = await findUserByEmail(emailUser)
 
-
   if (!user || user.password !== encodePassword) {
-    throw new HttpError('Unauthorized', HttpStatusCode.UNAUTHORIZED)
+    throw new HttpError("Unauthorized", HttpStatusCode.UNAUTHORIZED)
+  }
+
+  if (user.flag.includes("first-login")) {
+    auth = false
   }
 
   const payload = { name: user.name, email: user.email, role: user.type }
   const accessToken = jwt.sign(payload, SECRET)
 
   return {
-    auth: true,
+    auth,
     accessToken,
-    user: payload
+    user: payload,
   }
 }
 
 export const validateAccessToken = (authHeaders) => {
   const { SECRET } = process.env
-  const accessToken = authHeaders && authHeaders.split(' ')[1]
+  const accessToken = authHeaders && authHeaders.split(" ")[1]
 
-  const isAuthenticated = jwt.verify(
-    accessToken,
-    SECRET,
-    (err) => {
-      return !err
-    }
-  )
+  const isAuthenticated = jwt.verify(accessToken, SECRET, (err) => {
+    return !err
+  })
   return isAuthenticated
 }
