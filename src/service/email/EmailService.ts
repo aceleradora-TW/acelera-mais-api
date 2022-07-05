cac35e1fc8f6ec3ef8e89156661428987710c4a1
willian <willian.chris.89@gmail.com>
2022-07-05 14:48:46 -0300

    feat(service/EmailService):move logica de oauth2 para dentro de email service

--- src/service/email/EmailService.ts
+++ src/service/email/EmailService.ts
@@ -1,13 +1,55 @@
 import { NodemailerService } from "@service/nodemailer/NodemailerService"
+import { google } from "googleapis"
+import dotenv from "dotenv"
+dotenv.config()
 
-export const EmailService = () => {
+const OAuth2 = google.auth.OAuth2
+
+const {
+  NODEMAILER_EMAIL,
+  NODEMAILER_PASSWORD,
+  NODEMAILER_REDIRECT_URI,
+  NODEMAILER_CLIENT_ID,
+  NODEMAILER_CLIENT_SECRET,
+  NODEMAILER_REFRESH_TOKEN,
+} = process.env
+
+export const EmailService = async () => {
   const send = async (from, subject, email, content, bcc = "") => {
+    const oauth2Client = new OAuth2(
+      NODEMAILER_CLIENT_ID,
+      NODEMAILER_CLIENT_SECRET,
+      NODEMAILER_REDIRECT_URI
+    )
+
+    oauth2Client.setCredentials({
+      refresh_token: NODEMAILER_REFRESH_TOKEN,
+    })
+
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(token)
+      })
+    })
+
     const message = {
       from: from,
       to: email,
       bcc: bcc,
       subject: subject,
       text: content,
+      auth: {
+        type: "OAuth2",
+        user: NODEMAILER_EMAIL,
+        pass: NODEMAILER_PASSWORD,
+        accessToken,
+        clientId: NODEMAILER_CLIENT_ID,
+        clientSecret: NODEMAILER_CLIENT_SECRET,
+        refreshToken: NODEMAILER_REFRESH_TOKEN,
+      },
     }
 
     try {
--- src/service/nodemailer/NodemailerService.ts
+++ src/service/nodemailer/NodemailerService.ts
@@ -1,57 +1,8 @@
 import nodemailer from "nodemailer"
-import { google } from "googleapis"
-import dotenv from "dotenv"
-dotenv.config()
-
-const OAuth2 = google.auth.OAuth2
-
-const {
-  NODEMAILER_EMAIL,
-  NODEMAILER_PASSWORD,
-  NODEMAILER_REDIRECT_URI,
-  NODEMAILER_CLIENT_ID,
-  NODEMAILER_CLIENT_SECRET,
-  NODEMAILER_REFRESH_TOKEN,
-} = process.env
 
 export const NodemailerService = async () => {
-  const oauth2Client = new OAuth2(
-    NODEMAILER_CLIENT_ID,
-    NODEMAILER_CLIENT_SECRET,
-    NODEMAILER_REDIRECT_URI
-  )
-
-  oauth2Client.setCredentials({
-    refresh_token: NODEMAILER_REFRESH_TOKEN,
-  })
-
-  const accessToken = await new Promise((resolve, reject) => {
-    oauth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject(err)
-      }
-      resolve(token)
-    })
-  })
-
-  oauth2Client.on = () => {
-    if (NODEMAILER_REFRESH_TOKEN) {
-      console.log(NODEMAILER_REFRESH_TOKEN)
-    }
-    console.log(accessToken)
-  }
-
   const transport = nodemailer.createTransport({
     service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: NODEMAILER_EMAIL,
-      pass: NODEMAILER_PASSWORD,
-      accessToken,
-      clientId: NODEMAILER_CLIENT_ID,
-      clientSecret: NODEMAILER_CLIENT_SECRET,
-      refreshToken: NODEMAILER_REFRESH_TOKEN,
-    },
     tls: {
       rejectUnauthorized: false,
     },
